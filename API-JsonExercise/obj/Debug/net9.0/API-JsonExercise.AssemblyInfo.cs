e36ab8b5ebf31b72c37aa04f3f6fa7a8550c4080
Zach Brown <bigcountry66789@gmail.com>
2026-05-01 16:52:59 -0500

    Adding files

--- API-JsonExercise/obj/Debug/net9.0/API-JsonExercise.AssemblyInfo.cs
+++ API-JsonExercise/obj/Debug/net9.0/API-JsonExercise.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API-JsonExercise")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d57e677076281adfa97d6bd4305b5e2d77f39c49")]
 [assembly: System.Reflection.AssemblyProductAttribute("API-JsonExercise")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API-JsonExercise")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
